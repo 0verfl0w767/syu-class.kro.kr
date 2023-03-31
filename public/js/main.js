9e1b6e28f32aa2ba76ff22d16c0b4dd52d034aa9
0verfl0w767 <tkddbs7631@naver.com>
2023-04-01 01:44:00 +0900

    Fixed search for days logic
    ..

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,8 @@ function setInfo(datas, day) {
   document.getElementById("77").innerHTML = checkInfo(datas, day,"디지털 리터러시영역");
   document.getElementById("88").innerHTML = checkInfo(datas, day, "");
 
-  document.getElementById("daySelectCount").innerHTML = daySelectCount + "개";
+  // document.getElementById("daySelectCount").innerHTML = daySelectCount + "개";
+  document.getElementById("count").innerHTML = daySelectCount + "개";
 }
 function daySelectEvent(object) {
   daySelectCount = 0;
@@ -40,13 +41,18 @@ function daySelectEvent(object) {
 function checkCount(datas, day, className) {
   let count = 0;
   for (let i = 0; i < datas["api"].length; i++) {
-    if (datas["api"][i]["영역구분"] == className) {
-      if (day == "전체") {
+    if (datas["api"][i]["영역구분"] === className) {
+      if (day === "전체") {
         count++;
         daySelectCount++;
-      } else if (datas["api"][i]["수업시간/장소"].substr(0, 1) == day) {
+      } else if (datas["api"][i]["수업시간"].substr(0, 1) === day) {
         count++;
         daySelectCount++;
+      } else if (datas["api"][i]["수업시간"].split(",")[1] !== undefined) {
+        if (datas["api"][i]["수업시간"].split(",")[1].substr(0, 1) === day) {
+          count++;
+          daySelectCount++;
+        }
       }
     }
   }
@@ -54,52 +60,49 @@ function checkCount(datas, day, className) {
 }
 function changeDay(classInfo) {
   let day = 0;
-  switch (classInfo) {
-    case "월":
-      day = 1;
-      break;
-    case "화":
-      day = 2;
-      break;
-    case "수":
-      day = 3;
-      break;
-    case "목":
-      day = 4;
-      break;
-    case "금":
-      day = 5;
-      break;
-    default:
-      day = 0;
-  }
+  if (classInfo === "월")
+    day = 1
+  if (classInfo === "화")
+    day = 2
+  if (classInfo === "수")
+    day = 3
+  if (classInfo === "목")
+    day = 4
+  if (classInfo === "금")
+    day = 5
+  else
+    day = 0
   return day;
 }
 function checkInfo(datas, day, className) {
   let newData = [];
   let html_tag = "";
   for (let i = 0; i < datas["api"].length; i++) {
-    if (datas["api"][i]["영역구분"] == className) {
-      if (day == "전체") {
+    if (datas["api"][i]["영역구분"] === className) {
+      if (day === "전체") {
         newData.push(datas["api"][i]);
-      } else if (datas["api"][i]["수업시간/장소"].substr(0, 1) == day) {
+      } else if (datas["api"][i]["수업시간"].substr(0, 1) === day) {
         newData.push(datas["api"][i]);
+      } else if (datas["api"][i]["수업시간"].split(",")[1] !== undefined) {
+        if (datas["api"][i]["수업시간"].split(",")[1].substr(0, 1) === day) {
+          newData.push(datas["api"][i]);
+        }
       }
     }
   }
   newData.sort((a, b) => {
-    const dayA = a["수업시간/장소"].substr(0, 1);
-    const dayB = b["수업시간/장소"].substr(0, 1);
+    const dayA = a["수업시간"].substr(0, 1);
+    const dayB = b["수업시간"].substr(0, 1);
     if (changeDay(dayA) > changeDay(dayB)) return 1;
     if (changeDay(dayA) < changeDay(dayB)) return -1;
-    const timeA = parseInt(a["수업시간/장소"].substr(1, 2));
-    const timeB = parseInt(b["수업시간/장소"].substr(1, 2));
+    const timeA = parseInt(a["수업시간"].substr(1, 2));
+    const timeB = parseInt(b["수업시간"].substr(1, 2));
     if (timeA > timeB) return 1;
     if (timeA < timeB) return -1;
   });
   // newData.sort((a, b) => {
-  //   let aa = a["수업시간/장소"].substr(0, 1);
-  //   let bb = b["수업시간/장소"].substr(0, 1);
+  //   let aa = a["수업시간"].substr(0, 1);
+  //   let bb = b["수업시간"].substr(0, 1);
   //   if (changeDay(aa) < changeDay(bb)) return 1;
   //   if (changeDay(aa) > changeDay(bb)) return -1;
   // });
@@ -133,7 +136,7 @@ function checkInfo(datas, day, className) {
     //   <td><span style="color: white;">${newData[i]["영역구분"]}</span></td>
     //   <td><span style="color: yellow;">${newData[i]["학점"]}</span></td>
     //   <td><span style="color: #5f6062;">${newData[i]["교수명"]}</span></td>
-    //   <td><span style="color: yellow;">${newData[i]["수업시간/장소"]}</span></td>
+    //   <td><span style="color: yellow;">${newData[i]["수업시간"]}</span></td>
     // </tr>
     // `;
   }
